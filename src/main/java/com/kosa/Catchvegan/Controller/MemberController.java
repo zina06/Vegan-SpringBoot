8be291c36ad0dbd8314ea765bc2683cca8476228
IAMWith <hoons1007@naver.com>
2023-04-11 09:40:34 +0900

    시큐리티 제거

--- src/main/java/com/kosa/Catchvegan/Controller/MemberController.java
+++ src/main/java/com/kosa/Catchvegan/Controller/MemberController.java
@@ -1,16 +1,15 @@
 package com.kosa.Catchvegan.Controller;
 
+
 import com.kosa.Catchvegan.DTO.MemberDTO;
 import com.kosa.Catchvegan.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
 public class MemberController {
 
     @Autowired
@@ -18,8 +17,14 @@ public class MemberController {
     //@Autowired
     //private PasswordEncoder passwordEncoder;
 
+    @GetMapping("/")
+    public String main(){
+        return "success";
+    }
+
     @PostMapping("/member/join")
-    public String saveMember(@RequestBody MemberDTO memberdto) throws Exception{
+    public String saveMember(@RequestBody MemberDTO memberDTO) {
+        memberService.createMember(memberDTO);
         return "success";
     }
 
--- src/main/java/com/kosa/Catchvegan/Service/MemberService.java
+++ src/main/java/com/kosa/Catchvegan/Service/MemberService.java
@@ -1,7 +1,17 @@
 package com.kosa.Catchvegan.Service;
 
+import com.kosa.Catchvegan.DTO.MemberDTO;
+import com.kosa.Catchvegan.Mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberService {
+import java.util.List;
+
+
+public interface MemberService {
+
+    public List<MemberDTO> findAllMembers();
+
+    public void createMember(MemberDTO member);
+
 }
