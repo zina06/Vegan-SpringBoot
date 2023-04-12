ba0e975fbf338035c167a19e973ff2fa79c25737
IAMWith <hoons1007@naver.com>
2023-04-12 12:25:16 +0900

    ...

--- src/main/java/com/kosa/Catchvegan/Controller/MemberController.java
+++ src/main/java/com/kosa/Catchvegan/Controller/MemberController.java
@@ -26,8 +26,8 @@ public class MemberController {
     @PostMapping("/member/signup")
     public ResponseEntity<String> signup(@RequestBody MemberDTO memberDTO) {
         memberService.createMember(memberDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-        //return new ResponseEntity<>("signup", HttpStatus.OK);
+        //return ResponseEntity.status(HttpStatus.CREATED).build();
+        return new ResponseEntity<>("signup", HttpStatus.OK);
     }
 
     @GetMapping("/member/aftersignup")
